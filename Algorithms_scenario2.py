54145385e33e9bfb0f3804ae1b10f34ec17839c0
JaumeSaez <101106238+JaumeSaez@users.noreply.github.com>
2022-04-05 18:22:19 +0200

    Update Algorithms_scenario2.py

--- Algorithms_scenario2.py
+++ Algorithms_scenario2.py
@@ -11,7 +11,7 @@ from helpers import distance
 import networkx as nx
 import csv
 import cdlib
-from cdlib import algorithms
+from cdlib import algorithms, readwrite
 
 
 
@@ -72,7 +72,14 @@ while c<len(graph):
     while co < len(graph[c]):
 
         G.add_weighted_edges_from([(graph[c][co])])
-        coms = algorithms.condor(G)
+        
         
         co+=1
+
+
+    com = algorithms.louvain(G)
+    readwrite.write_community_json(com, "communitieslouvian.json")
+
+    readwrite.write_community_csv(com, "communities.csv", ",")
+
     c+=1
