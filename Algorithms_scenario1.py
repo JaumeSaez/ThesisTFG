b31142eabb3128771f7661b26cbdaf0aa82ce8b2
JaumeSaez <101106238+JaumeSaez@users.noreply.github.com>
2022-04-06 12:53:05 +0200

    Update Algorithms_scenario1.py

--- Algorithms_scenario1.py
+++ Algorithms_scenario1.py
@@ -31,7 +31,7 @@ def weight_(result_obj):
     
     return weight
 
-def add_edge(ac1_id, ac1_lat, ac1_lon, ac2_id, ac2_lat, ac2_lon,edge):
+def add_edge(ac1_id, ac1_lat, ac1_lon, ac2_id, ac2_lat, ac2_lon,edge,time):
     I = nx.Graph()    
     result = distance(ac1_lat, ac1_lon, ac2_lat, ac2_lon)
     result = np.squeeze(np.asarray(result))
@@ -49,40 +49,23 @@ time_df = df['time '].unique()
 count = 0
 edge = []
 graph = []
-while count < len(time_df):
+G = nx.MultiGraph()
+while count < len(time_df) :
     sub_df = df.loc[(df['time '] == time_df[count])]
     alt_df_numpy = sub_df.to_numpy()
-    G = nx.Graph()
+  
     for position1 in alt_df_numpy:
         for position2 in alt_df_numpy:
             if position1[1] != position2[1]:
                 #print(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4])
-                gra= add_edge(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4],edge)
-    edge = []
-    
-    
-    graph.append(gra)
+                gra= add_edge(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4],edge,time_df[count])
+                G.add_weighted_edges_from(gra)
+                edge = []
 
-
+    graph = []
     count = count + 1
+print (G.edges.data('weight'))
+com = algorithms.louvain(G)
+readwrite.write_community_json(com, "communitieslouvian.json")
 
-
-c=0
-comunity = []
-while c<len(graph):
-    co = 0
-    while co < len(graph[c]):
-
-        G.add_weighted_edges_from([(graph[c][co])])
-
-        
-        co+=1
-
-    
-    com = algorithms.louvain(G)
-    readwrite.write_community_json(com, "communitieslouvian.json")
-
-    readwrite.write_community_csv(com, "communities.csv", ",")
-
-    
-    c+=1
+readwrite.write_community_csv(com, "communities.csv", ",")
