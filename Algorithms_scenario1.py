ca676c484abb5eb6f74e991f9b0aede5ec30c197
JaumeSaez <101106238+JaumeSaez@users.noreply.github.com>
2022-04-11 13:29:05 +0200

    Update Algorithms_scenario1.py

--- Algorithms_scenario1.py
+++ Algorithms_scenario1.py
@@ -12,7 +12,7 @@ import networkx as nx
 import csv
 import cdlib
 from cdlib import algorithms, readwrite
-
+import json
 
 
 
@@ -31,7 +31,7 @@ def weight_(result_obj):
     
     return weight
 
-def add_edge(ac1_id, ac1_lat, ac1_lon, ac2_id, ac2_lat, ac2_lon,edge,time):
+def add_edge(ac1_id, ac1_lat, ac1_lon, ac2_id, ac2_lat, ac2_lon,edge):
     I = nx.Graph()    
     result = distance(ac1_lat, ac1_lon, ac2_lat, ac2_lon)
     result = np.squeeze(np.asarray(result))
@@ -49,23 +49,35 @@ time_df = df['time '].unique()
 count = 0
 edge = []
 graph = []
-G = nx.MultiGraph()
-while count < len(time_df) :
+while count < len(time_df):
     sub_df = df.loc[(df['time '] == time_df[count])]
     alt_df_numpy = sub_df.to_numpy()
-  
+    G = nx.Graph()
     for position1 in alt_df_numpy:
         for position2 in alt_df_numpy:
             if position1[1] != position2[1]:
                 #print(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4])
-                gra= add_edge(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4],edge,time_df[count])
-                G.add_weighted_edges_from(gra)
-                edge = []
+                gra= add_edge(position1[1],position1[3],position1[4],position2[1],position2[3],position2[4],edge)
+    edge = []
+    
+    
+    graph.append(gra)
+    #print(time_df[count])
+    
+    c=0
+    G=nx.Graph()
+    #print(graph)
+    while c<len(graph[count]):
+        G.add_weighted_edges_from([(graph[count][c])])
+        
+        
+        c+=1
+    #print(G.edges.data('weight'))
+    com = algorithms.louvain(G)
+    comunities = str(time_df[count])
+    comunities = comunities+".json"
 
-    graph = []
+    readwrite.write_community_json(com, comunities)
+    
+
     count = count + 1
-print (G.edges.data('weight'))
-com = algorithms.louvain(G)
-readwrite.write_community_json(com, "communitieslouvian.json")
-
-readwrite.write_community_csv(com, "communities.csv", ",")
